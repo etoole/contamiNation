17504d07e54fd2542165d4153af8aa8be052e319
etoole <erictoole@gmail.com>
2017-11-30 12:42:01 -0500

    update

--- geocoding/geocode.py
+++ geocoding/geocode.py
@@ -7,7 +7,7 @@ zip_pattern = re.compile('[0-9]{5}(?!.)')
 GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
 api_key = 'AIzaSyDrawt5ZWXPR7eg0ihu3t6p9A_Yrm1ss0E'
 
-with open(os.path.join(os.path.abspath('../contamiNation'),'outputbatch_0.json')) as results:
+with open(os.path.join(os.path.abspath('../contamiNation'),'outputbatch_0_cut.json')) as results:
     results = json.load(results)
 
     for result in results:
@@ -31,12 +31,15 @@ with open(os.path.join(os.path.abspath('../contamiNation'),'outputbatch_0.json')
                     if res['status'] !='ZERO_RESULTS':
 
                         address_components = res['results'][0]['address_components']
-                        for components in address_components:
-                            m = re.match(zip_pattern, components['long_name'])
 
-                            if m:
-                                google_maps_zip_code = m.group()
-                                google_maps_zip_short = re.match('[0-9]{3}', google_maps_zip_code)
+                        for components in address_components:
+                            if components['types'][0] == 'postal_code':
+
+                                m = re.match(zip_pattern, components['long_name'])
+                                if m:
+                                    google_maps_zip_code = m.group()
+                                    google_maps_zip_short = re.match('[0-9]{3}', google_maps_zip_code)
+
 
                         latitude = res['results'][0]['geometry']['location']['lat']
                         longitude = res['results'][0]['geometry']['location']['lng']
@@ -44,6 +47,8 @@ with open(os.path.join(os.path.abspath('../contamiNation'),'outputbatch_0.json')
 
                         epa_zip_short = re.match('[0-9]{3}', result['Zip Code'])
                         if epa_zip_short.group() == google_maps_zip_short.group():
+                            print(epa_zip_short.group())
+                            print(google_maps_zip_short.group())
                                 #write geojson with google maps data and pwsid at end
 
                             geojson = {
@@ -66,16 +71,19 @@ with open(os.path.join(os.path.abspath('../contamiNation'),'outputbatch_0.json')
                             print(geojson)
                         else:
                                 #write geojson with epa zipcode. make googlemaps request for longlat and use epa name & pwsid
+                            print(epa_zip_short.group())
+                            print(google_maps_zip_short.group())
                             zipcode_marker = {'address' : result['Zip Code'],
                             'key': api_key
                             }
 
                             zip_req = requests.get(GOOGLE_MAPS_API_URL, params=zipcode_marker)
-                            zip_res = req.json()
-                            zip_latitude = zip_res['results'][0]['geometry']['location']['lat']
-                            zip_longitude = zip_res['results'][0]['geometry']['location']['lng']
-                            zip_coordinates = [zip_longitude, zip_latitude]
-
+                            zip_res = zip_req.json()
+                            if zip_res['status'] != 'ZERO_RESULTS':
+                                zip_latitude = zip_res['results'][0]['geometry']['location']['lat']
+                                zip_longitude = zip_res['results'][0]['geometry']['location']['lng']
+                                zip_coordinates = [zip_longitude, zip_latitude]
+                                print(zip_coordinates)
                             geojson = {
                                   "type": "Feature",
                                   "geometry": {
@@ -94,7 +102,8 @@ with open(os.path.join(os.path.abspath('../contamiNation'),'outputbatch_0.json')
                                 }
                             geojson_list.append(geojson)
                             print(geojson)
-json.dump(geojson_list, open(os.path.join(os.path.abspath('../contamiNation/geocoding/'),'markers_0.json'),'w'), indent=4)
+
+json.dump(geojson_list, open(os.path.join(os.path.abspath('../contamiNation/geocoding/'),'test_markers.json'),'w'), indent=4)
 
 
 
