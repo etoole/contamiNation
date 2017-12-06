42de521db00d93e2703510ed746784cf1cccfad1
etoole <erictoole@gmail.com>
2017-12-06 14:20:10 -0500

    geocoding almost finished; next upload to map

--- geocoding/geocode_debug.py
+++ geocoding/geocode_debug.py
@@ -7,7 +7,7 @@ zip_pattern = re.compile('[0-9]{5}(?!.)')
 GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
 api_key = 'AIzaSyDrawt5ZWXPR7eg0ihu3t6p9A_Yrm1ss0E'
 
-with open(os.path.join(os.path.abspath('../'),'outputbatch_6.json')) as results:
+with open(os.path.join(os.path.abspath('../geocoding/'),'list_index_error.json')) as results:
     results = json.load(results)
 
     for result in results:
@@ -30,6 +30,7 @@ with open(os.path.join(os.path.abspath('../'),'outputbatch_6.json')) as results:
 
                     if res['status'] !='ZERO_RESULTS':
                         if len(res['results']) > 0:
+
                             address_components = res['results'][0]['address_components']
 
                             for components in address_components:
@@ -101,65 +102,4 @@ with open(os.path.join(os.path.abspath('../'),'outputbatch_6.json')) as results:
                                       }
                                     }
                                 geojson_list.append(geojson)
-                            print(geojson)
-
-json.dump(geojson_list, open(os.path.join(os.path.abspath('../geocoding/'),'markers_6.json'),'w'), indent=4)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#             components['long_name']
-#             if zip_pattern in long_name:
-#                 print(long_name)
-#
-# for element in name:
-#      m = re.match("(^[A-Z]\d[A-Z]\d)", element)
-#      if m:
-#         print(m.groups())
-
-
-
-
-#         geocode_list.append(res)
-#
-# print(geocode_list)
-# json.dump(geocode_list, open('test_geocode_results.json', 'w'), indent=4)
-
-
-
-
-
-
-
-
-
-# params = {
-#     'address': '221B Baker Street, London, United Kingdom',
-#     'sensor': 'false',
-#     'region': 'uk'
-# }
-#
-# # Do the request and get the response data
-# req = requests.get(GOOGLE_MAPS_API_URL, params=params)
-# res = req.json()
-#
-# # Use the first result
-# result = res['results'][0]
-#
-# geodata = dict()
-# geodata['lat'] = result['geometry']['location']['lat']
-# geodata['lng'] = result['geometry']['location']['lng']
-# geodata['address'] = result['formatted_address']
-
-#print('{address}. (lat, lng) = ({lat}, {lng})'.format(**geodata))
-# 221B Baker Street, London, Greater London NW1 6XE, UK. (lat, lng) = (51.5237038, -0.1585531)
+                                print(geojson)
--- json_grouper.py
+++ json_grouper.py
@@ -2,7 +2,6 @@ import json
 from itertools import zip_longest
 
 def grouper(iterable, n, fillvalue=None):
-    "Collect data into fixed-length chunks or blocks"
     # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
     args = [iter(iterable)] * n
     return zip_longest(fillvalue=fillvalue, *args)
